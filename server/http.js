c03a895eddb739c6491261fc269f6c64e81a3e73
bin <bin.zhu@ge.com>
2017-06-25 17:15:14 +0800

    add mail and schedule

--- server/http.js
+++ server/http.js
@@ -3,6 +3,51 @@ var fs = require('fs');
 var url = require('url');
 var querystring = require('querystring');
 var util = require('util');
+var schedule = require('node-schedule');
+var nodemailer = require('nodemailer');
+
+var config = getConfig();
+console.log(config)
+function getConfig(){
+	return JSON.parse(fs.readFileSync('config.json', 'utf8'));
+}
+
+function sendMail(){
+	var transporter = nodemailer.createTransport({
+	    //https://github.com/andris9/nodemailer-wellknown#supported-services support service 
+	    service: config.service,
+	    port: config.port, // SMTP port
+	    //secureConnection: true,	// disable this, deploy to cloud, it doesn't work if enable this, maybe block by cloud
+	    auth: {
+	        user: config.user,
+	        pass: config.pass
+	    }
+	});
+
+	var content = fs.readFileSync('items.json', 'utf8');
+	// setup e-mail data with unicode symbols
+	var mailOptions = {
+	    from: config.user,
+	    to: config.to,
+	    subject: 'timecard backup'+new Date(),
+		
+	    //text: 'Hello world ?',
+	    html: content
+	};
+
+	// send mail with defined transport object
+	console.log("send mail start at "+new Date())
+	//console.log(mailOptions)
+	transporter.sendMail(mailOptions, function(error, info){
+	    if(error){
+	        console.log("send mail error:"+error);
+	    } else{
+	    	console.log('Message sent at ' + new Date()+" :"+info.response);
+	    }
+	});
+};
+
+schedule.scheduleJob(JSON.parse(config.schedule), sendMail);
 
 http.createServer(function(req, res){
    
